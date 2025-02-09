a412c3943a98f0de3365888efa56a21aff2d2b8b
jinxIsOp <154748750+jinxIsOp@users.noreply.github.com>
2025-02-09 15:35:06 +0100

    testing the examples

--- Examples/example_cpu_task.py
+++ Examples/example_cpu_task.py
@@ -1,4 +1,9 @@
-from SmartExecutor.core import run
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from smartexecutor.core import run
 
 def heavy_computation(n):
     total = 0
@@ -6,9 +11,7 @@ def heavy_computation(n):
         total += i ** 2
     return total
 
-
-# using the cpu for heavy computation for the test of the smartexecuter
 if __name__ == "__main__":
     n = 1000000
     result = run(heavy_computation, n)
-    print("Résultat du calcul intensif (CPU-bound):", result)
+    print("CPU-bound task result:", result)
